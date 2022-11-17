647ddef314b9112a3e4e2da3adff23f803b29e53
Thibault Serdet <tserdet@k0r3p5.42mulhouse.fr>
2022-11-17 12:13:47 +0100

    o

--- ft_itoa.c
+++ ft_itoa.c
@@ -10,11 +10,12 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+//http://villemin.gerard.free.fr/aInforma/081Chiff.htm
 #include "libft.h"
 
-int nombre_chiffre(n)
+int	nombre_chiffre(int n)
 {
-   	int count;
+	int	count;
 
 	count = 0;
 	if (n <= 0)
@@ -22,20 +23,20 @@ int nombre_chiffre(n)
 		count++;
 		n = n * -1;
 	}
-   	while(n!=0)  
-   	{  
-   	    n=n/10;  
-   	    count++;
-   	}
-	return(count);
-} 
+	while (n != 0)
+	{
+		n = n / 10;
+		count++;
+	}
+	return (count);
+}
 
 char	convertion(int p)
 {
 	char	n;
 
 	n = 48 + p;
-	return(n);
+	return (n);
 }
 
 char	*strcpyy(char *dest, char *src)
@@ -52,30 +53,30 @@ char	*strcpyy(char *dest, char *src)
 	return (dest);
 }
 
-char	*cas_part(char	*str, int	m)
+char	*cas_part(char *str, int m)
 {
 	if (m == 0 || m == -0)
 	{
 		str = malloc(sizeof(char) * 2);
 		if (str == NULL)
-			return(0);
+			return (0);
 		strcpyy(str, "0");
 	}
 	if (m == -2147483648)
 	{
 		str = malloc(sizeof(char) * 12);
 		if (str == NULL)
-			return(0);
+			return (0);
 		strcpyy(str, "-2147483648");
 	}
-	return(str);
+	return (str);
 }
 
-char *ft_itoa(int m)
+char	*ft_itoa(int m)
 {
-	char *str;
-	int	c;
-	int	i;
+	char	*str;
+	int		c;
+	int		i;
 
 	i = nombre_chiffre(m);
 	if (m == 0 || m == -0 || m == -2147483648)
@@ -84,19 +85,19 @@ char *ft_itoa(int m)
 	{
 		str = malloc(sizeof(char) * nombre_chiffre(m) + 1);
 		if (str == NULL)
-			return(0);
+			return (0);
 		str[i] = '\0';
 		if (m < 0)
-			{
-				str[0] = '-';
-				m = m * -1;
-			}
-		while(m > 0)
+		{
+			str[0] = '-';
+			m = m * -1;
+		}
+		while (m > 0)
 		{
 			c = m % 10;
 			str[--i] = convertion(c);
-			m = (m - c) / 10; //http://villemin.gerard.free.fr/aInforma/081Chiff.htm
+			m = (m - c) / 10;
 		}
 	}
-	return(str);
+	return (str);
 }
